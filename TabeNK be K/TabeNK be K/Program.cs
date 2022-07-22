b1d844eea6342c51548951e364f7bf54583034a7
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-07-22 02:38:49 -0700

    Add files via upload

---
+++ TabeNK be K/TabeNK be K/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabeNK_be_K
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(combination(8, 5));
+            Console.ReadKey();
+        }
+        public static int combination(int n, int k)
+        {
+            if (k == 0 || n == k)
+            {
+                return 1;
+            }
+            else
+            {
+                return (combination(n-1 , k-1) + combination(n-1 , k));
+            }
+
+        }
+        
+    }
+    
+}
